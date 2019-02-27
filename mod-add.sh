c55dc790ca1dbdce0e2d59049aaa074ba3c2419e
Robb <hxh.robb@gmail.com>
2019-02-27 14:55:05 +0800

    add mod_name parameter

--- mod-add.sh
+++ mod-add.sh
@@ -6,6 +6,8 @@ for arg in $@; do
       git_url="$arg"
     elif [ -z "$git_branch" ]; then
       git_branch="$arg"
+    elif [ -z "$mod_name" ]; then
+      mod_name="$arg
     fi
   fi
 done
@@ -20,7 +22,8 @@ echo "==========================="
 test -z "${git_url}" && echo "Please input the git repo address:" || echo "git repo address:[${git_url}]"
 test -z "${git_url}" && read git_url
 
-mod=$(echo "${git_url}"|awk -F'\:|\/|\.git' '{print tolower($(NF-1))}' 2>/dev/null)
+#mod=$(echo "${git_url}"|awk -F'\:|\/|\.git' '{print tolower($(NF-1))}' 2>/dev/null)
+[ -n "${mod_name}" ] && mod="${mod_name}" || mod=$(echo "${git_url}"|awk -F'\:|\/|\.git' '{print tolower($(NF-1))}' 2>/dev/null)
 test -d modules/${mod} && echo "[${mod}] is already exists" && exit 1
 
 test -z "${git_branch}" && echo "Please input the branch to checkout:" || echo "git branch:[${git_branch}]"
